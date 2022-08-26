a02f2365a21d6c76d39405e077f06c4168dbc268
msr <m@gmail.com>
2022-08-26 14:25:25 +0530

    removed some pngs

--- src/main/java/com/fis/SampleProjectApplication.java
+++ src/main/java/com/fis/SampleProjectApplication.java
@@ -1,17 +1,19 @@
 package com.fis;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Qr Code", description = "contains the apis", version = "1.0"))
 public class SampleProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
 	}
-
 
 	@Bean
 	RestTemplate restTemplate()
